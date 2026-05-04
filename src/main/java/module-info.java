1ab5994c079a7be7baa3a60b8a80c500866905a6
ranya-b <berkane.ranya04@gmail.com>
2026-05-04 19:32:23 +0200

    maj de la version de Java + début de la tentative de déploiement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.ranyar.castlefightgraphic1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
+    requires java.sql;      
     opens com.ranyar.castlefightgraphic1 to javafx.fxml;
     exports com.ranyar.castlefightgraphic1;
     opens com.ranyar.castlefightgraphic1.controllers to javafx.fxml;
